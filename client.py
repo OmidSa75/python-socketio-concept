81c0235f3799dd634c07d20b10b8429773ef03eb
parstech <parstech@parstech-1.(none)>
2022-01-02 16:52:09 +0330

    complete client

--- client.py
+++ client.py
@@ -13,14 +13,14 @@ from aiohttp import web
 class SocketClient:
     sio = socketio.AsyncClient()
 
-    async def add(self):
-        await self.sio.emit('add', {'response': "add camera to server"})
+    async def add_camera(self, cam: dict):
+        await self.sio.emit('add_camera', cam)
 
-    async def edit(self):
-        await self.sio.emit('edit', {'response': "edit camera from server"})
+    async def edit_camera(self, cam: dict):
+        await self.sio.emit('edit_camera', cam)
 
-    async def remove(self):
-        await self.sio.emit('remove', {'response': "remove camera from server"})
+    async def remove_camera(self, cam: dict):
+        await self.sio.emit('remove_camera', cam)
 
     # @sio.event
     # async def connect(self):
@@ -32,17 +32,31 @@ class SocketClient:
             logging.info(f"Socket connected")
 
         @self.sio.event
-        async def my_message(data):
+        async def response(data):
             print('message received with ', data)
-            await self.sio.emit('my response', {'response': 'my response'})
 
         @self.sio.event
         async def disconnect():
             logging.info(f"Socket disconnected")
 
+        @self.sio.on('execute')
+        async def execute():
+            logging.info("Executing the process . . .")
+            await self.execute()
+
+        @self.sio.on('show_logs')
+        async def show_logs(logs):
+            logging.info(pformat(logs))
+
+    async def execute(self):
+        await self.sio.emit('execute')
+
+    async def terminate(self):
+        await self.sio.emit('terminate')
+
     async def run(self):
         # self.call_backs()
-        await self.sio.connect("http://localhost:8001")
+        await self.sio.connect("http://localhost:8000")
         await self.sio.wait()
 
 # import asyncio
@@ -68,11 +82,16 @@ class SocketClient:
 #     await sio.connect('http://localhost:8001')
 #     await sio.wait()
 
+
 if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)s - %(filename)s|%(funcName)s - %(levelname)s - %(message)s",
                         level=logging.DEBUG)
+    with open('cameras.json', 'r') as f:
+        cam_samples = json.load(f)
+
+    import threading
     client = SocketClient()
     client.call_backs()
-    asyncio.run(client.run())
+    threading.Thread(target=asyncio.run, args=[(client.run())]).start()
     # asyncio.run(main())
 
