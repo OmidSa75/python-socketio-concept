aace30ffc167ea0556245081072dd8aa2cbb6533
parstech <parstech@parstech-1.(none)>
2022-04-03 14:15:16 +0430

    class base sockets

--- client.py
+++ client.py
@@ -8,13 +8,24 @@ import time
 
 import socketio
 from aiohttp import web
+# import nest_asyncio
+# nest_asyncio.apply()
 
 
 class SocketClient:
-    sio = socketio.AsyncClient()
+    client_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(client_loop)
+    sio = socketio.AsyncClient(logger=True)
 
     async def add_camera(self, cam: dict):
-        await self.sio.emit('add_camera', cam)
+        await self.sio.emit('add_camera', cam, callback=self.callback_fn())
+        # task = asyncio.create_task(self.sio.emit('add_camera', cam))
+        logging.info("Sent Message")
+        return
+
+    @staticmethod
+    def callback_fn():
+        logging.info("\033[1;34m" + "this is the callback function "+"\033[0;0m")
 
     async def edit_camera(self, cam: dict):
         await self.sio.emit('edit_camera', cam)
@@ -22,14 +33,16 @@ class SocketClient:
     async def remove_camera(self, cam: dict):
         await self.sio.emit('remove_camera', cam)
 
-    # @sio.event
-    # async def connect(self):
-    #     logging.info("socket connected")
+    async def sending_cameras(self):
+        while True:
+            time.sleep(5)
+            await self.add_camera({'cam':'OmidSa75'})
 
     def call_backs(self):
         @self.sio.event
         async def connect():
             logging.info(f"Socket connected")
+            # await self.sio.emit('send_message', {'message':"I am client"})
 
         @self.sio.event
         async def response(data):
@@ -48,6 +61,13 @@ class SocketClient:
         async def show_logs(logs):
             logging.info(pformat(logs))
 
+        @self.sio.on('send_message')
+        async def send_message(data):
+            if data:
+                logging.info('Client received a message: '+ str(data))
+            await asyncio.sleep(1)
+            await self.sio.emit('send_message', {'message': "I am client"})
+
     async def execute(self):
         await self.sio.emit('execute')
 
@@ -56,31 +76,11 @@ class SocketClient:
 
     async def run(self):
         # self.call_backs()
-        await self.sio.connect("http://localhost:8000")
+        await self.sio.connect("http://localhost:8001")
         await self.sio.wait()
 
-# import asyncio
-# import socketio
-#
-# sio = socketio.AsyncClient()
-#
-# @sio.event
-# async def connect():
-#     print('connection established')
-#     await sio.emit('connect')
-#
-# @sio.event
-# async def my_message(data):
-#     print('message received with ', data)
-#     await sio.emit('my response', {'response': 'my response'})
-#
-# @sio.event
-# async def disconnect():
-#     print('disconnected from server')
-#
-# async def main():
-#     await sio.connect('http://localhost:8001')
-#     await sio.wait()
+    def run_from_thread(self):
+        self.client_loop.run_until_complete(self.run())
 
 
 if __name__ == '__main__':
@@ -92,6 +92,7 @@ if __name__ == '__main__':
     import threading
     client = SocketClient()
     client.call_backs()
-    threading.Thread(target=asyncio.run, args=[(client.run())]).start()
-    # asyncio.run(main())
+    threading.Thread(target=client.run_from_thread).start()
+    # threading.Thread(target=asyncio.run, args=[(client.run())]).start()
+    # asyncio.run(client.run())
 
--- server.py
+++ server.py
@@ -1,32 +1,39 @@
 import asyncio
-import atexit
-import json
 import logging
 import threading
 from pprint import pformat
-import time
 
 import socketio
 from aiohttp import web
+# import nest_asyncio
+# nest_asyncio.apply()
 
 
 class SocketServer:
-    sio = socketio.AsyncServer(async_mode="aiohttp", cors_allowed_origins="*")
+    sio = socketio.AsyncServer(async_mode="aiohttp", cors_allowed_origins="*", logger=True, engineio_logger=True)
 
-    def aiohttp_server(self):
+    app = web.Application()
+    sio.attach(app)
+
+    @classmethod
+    def aiohttp_server(cls):
         app = web.Application()
-        self.sio.attach(app)
+        cls.sio.attach(app)
         runner = web.AppRunner(app)
         return runner
 
-    def run(self, runner):
+    def run_server(self, host, port, runner):
         self.call_backs()
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(runner.setup())
-        site = web.TCPSite(runner, "0.0.0.0", 8001)
+        site = web.TCPSite(runner, host, port)
         loop.run_until_complete(site.start())
         loop.run_forever()
+
+    def run(self):
+        self.call_backs()
+        web.run_app(self.app,host="0.0.0.0", port=8001)
 
     def call_backs(self):
         @self.sio.event
@@ -37,10 +44,12 @@ class SocketServer:
         async def disconnect(sid):
             logging.info(f"Socket from {str(sid)} is disconnected")
 
-        @self.sio.on("add")
+        @self.sio.on("add_camera")
         async def add(sid, data):
             if data:
-                logging.info(f'Received data: {pformat(data, indent=4)} from client.')
+                logging.info("\033[1;34m" + f'Received data: {pformat(data, indent=4)} from client.' + "\033[0;0m")
+
+            return {"status": "OK"}
 
         @self.sio.on("edit")
         async def edit(sid, data):
@@ -52,9 +61,19 @@ class SocketServer:
             if data:
                 logging.info(f"Removed data: {pformat(data, indent=4)}")
 
+        @self.sio.on('send_message')
+        async def send_message(sid, data):
+            if data:
+                logging.info('Server get the message: '+ str(data))
+            await asyncio.sleep(1)
+            await self.sio.emit('send_message', {"message":'Sever sending a message'})
+
 
 if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)s - %(filename)s|%(funcName)s - %(levelname)s - %(message)s",
                         level=logging.DEBUG)
     server = SocketServer()
-    server.run(server.aiohttp_server())
+    # server.run(server.aiohttp_server())
+    # server.run()
+    runner = server.aiohttp_server()
+    threading.Thread(target=server.run_server, args=['0.0.0.0', 8001, runner]).start()
